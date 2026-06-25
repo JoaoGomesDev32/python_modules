a5961cb210b5b63c8460e5bf25949c0f77e856ef
Joao Felipe Gomes Da Silva <joagomes@c1r5s6.42lisboa.com>
2026-06-25 12:55:30 +0100

    fix ex1 type hints

--- module01/ex1/ft_garden_data.py
+++ module01/ex1/ft_garden_data.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 class Plant:
+    name: str
+    height: int
+    age: int
+
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
