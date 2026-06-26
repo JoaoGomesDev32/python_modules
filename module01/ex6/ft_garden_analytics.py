4e12e1c6c6886f571b6e4f8031dc1a50f257604d
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-06-26 14:18:57 +0100

    feat: nested class stats

--- module01/ex6/ft_garden_analytics.py
+++ module01/ex6/ft_garden_analytics.py
@@ -1,16 +1,30 @@
 class Plant:
+    class Stats:
+        def __init__(self) -> None:
+            self._grow_count: int = 0
+            self._age_count: int = 0
+            self._show_count: int = 0
+
+        def display(self) -> None:
+            print(f"Stats: {self._grow_count} grow, "
+                  f"{self._age_count} age, "
+                  f"{self._show_count} show")
+
     def __init__(self, name: str, height: float, num_days: int,
                  grow_rate: float = 2.1) -> None:
         self.name = name
         self._height = height
         self._num_days = num_days
         self.grow_rate = grow_rate
+        self._stats = Plant.Stats()
 
     def grow(self) -> None:
         self._height += self.grow_rate
+        self._stats._grow_count += 1
 
     def age(self) -> None:
         self._num_days += 1
+        self._stats._age_count += 1
 
     def get_height(self) -> float:
         return self._height
@@ -54,6 +68,7 @@ class Plant:
         _height = round(self._height, 1)
         print(f"{self.name}: {_height:.1f}cm,"
               f" {self._num_days} days old")
+        self._stats._show_count += 1
 
 
 if __name__ == "__main__":
@@ -61,3 +76,11 @@ if __name__ == "__main__":
     print(Plant.is_older_than_year(400))
     anon = Plant.create_anonymous()
     anon.show()
+    lis = Plant("Lis", 12.5, 50)
+    for i in range(10):
+        lis.grow()
+    for i in range(12):
+        lis.age()
+    for i in range(5):
+        lis.show()
+    lis._stats.display()
