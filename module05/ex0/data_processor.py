a15192569d63bb68f6ec23938a418652fc52ecba
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-22 16:10:14 +0100

    data_processor done

--- module05/ex0/data_processor.py
+++ module05/ex0/data_processor.py
@@ -111,18 +111,41 @@ class LogProcessor(DataProcessor):
 if __name__ == "__main__":
     print("=== Code Nexus - Data Processor ===\n")
 
+    print("Testing Numeric Processor...")
     numeric = NumericProcessor()
-    print(f"Trying to validate input '42': {numeric.validate(42)}")
-    print(f"Trying to validate input 'Hello': {numeric.validate('Hello')}")
-
-    print("Test invalid ingestion of string 'foo' without prior validation:")
+    print(f" Trying to validate input '42': {numeric.validate(42)}")
+    print(f" Trying to validate input 'Hello': {numeric.validate('Hello')}")
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
     try:
         numeric.ingest('foo')  # type: ignore
     except ValueError as e:
-        print(f"Got exception: {e}")
-
+        print(f" Got exception: {e}")
+    print(" Processing data: [1, 2, 3, 4, 5]")
     numeric.ingest([1, 2, 3, 4, 5])
-    print("Extracting 3 values...")
+    print(" Extracting 3 values...")
     for _ in range(3):
         rank, value = numeric.output()
-        print(f"Numeric value {rank}: {value}")
+        print(f" Numeric value {rank}: {value}")
+
+    print("\nTesting Text Processor...")
+    text = TextProcessor()
+    print(f" Trying to validate input '42': {text.validate(42)}")
+    print(" Processing data: ['Hello', 'Nexus', 'World']")
+    text.ingest(['Hello', 'Nexus', 'World'])
+    print(" Extracting 1 value...")
+    rank, value = text.output()
+    print(f" Text value {rank}: {value}")
+
+    print("\nTesting Log Processor...")
+    log = LogProcessor()
+    print(f" Trying to validate input 'Hello': {log.validate('Hello')}")
+    logs = [
+        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+    ]
+    print(f" Processing data: {logs}")
+    log.ingest(logs)
+    print(" Extracting 2 values...")
+    for _ in range(2):
+        rank, value = log.output()
+        print(f" Log entry {rank}: {value}")
