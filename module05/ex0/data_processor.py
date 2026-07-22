9eb73184f0880a329d476b29a65d15d229a65020
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-22 15:15:41 +0100

    NumericProcessor class done

--- module05/ex0/data_processor.py
+++ module05/ex0/data_processor.py
@@ -19,12 +19,31 @@ class DataProcessor(ABC):
 
 
 class NumericProcessor(DataProcessor):
-    
-    def validate(self, data):
-        return super().validate(data)
+    def __init__(self) -> None:
+        self._storage: list[str] = []
+        self._rank: int = 0
 
-    def ingest(self, data: int | float):
-        return super().ingest(data)
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        if isinstance(data, list):
+            return all(isinstance(x, (int, float)) for x in data)
+        return False
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
+            raise ValueError("Improper numeric data")
+        if isinstance(data, list):
+            for item in data:
+                self._storage.append(str(item))
+        else:
+            self._storage.append(str(data))
+
+    def output(self) -> tuple[int, str]:
+        value = self._storage.pop(0)
+        rank = self._rank
+        self._rank += 1
+        return (rank, value)
 
 
 class TextProcessor(DataProcessor):
