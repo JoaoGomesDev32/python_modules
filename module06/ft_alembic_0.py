61621472bacad86e07939ef435221cf02f98092e
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-24 11:16:23 +0100

    ft_alembic_0

--- module06/ft_alembic_0.py
+++ module06/ft_alembic_0.py
@@ -2,4 +2,4 @@ import elements
 
 print("=== Alembic 0 ===")
 print("Using 'import ...' structure to access elements.py")
-print(f"Testing create_fire: {elements.create_water()}")
+print(f"Testing create_fire: {elements.create_fire()}")
