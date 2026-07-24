f0e22b8419c9645355baf1b39b37b858700f31af
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-24 12:19:38 +0100

    implement distillations_0

--- module06/alchemy/__init__.py
+++ module06/alchemy/__init__.py
@@ -1,5 +1,8 @@
 from .elements import create_air
+# from .potions import healing_potion, strength_potion
 
 __all__ = [
-    "create_air"
+    "create_air",
+#    "healing_potion",
+#    "strength_potion"
 ]
