e2c6b8fb8b5d39301ff839281113ef245b5ca976
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-07-25 10:52:44 +0100

    flake8 fixed

--- module06/alchemy/transmutation/recipes.py
+++ module06/alchemy/transmutation/recipes.py
@@ -2,6 +2,7 @@ from ..elements import create_air
 from alchemy.potions import strength_potion
 import elements
 
+
 def lead_to_gold() -> str:
     return (
         f"Recipe transmuting Lead to Gold: brew '{create_air()}'"
