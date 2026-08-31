2314eef8ddeebc9a671640a3250fcf6d7de7a32a
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-31 15:05:28 +0100

    create class Aquabub

--- module07/ex0/factory.py
+++ module07/ex0/factory.py
@@ -30,3 +30,11 @@ class Pyrodon(Creature):
         return f"{self.name} uses Flamethrower!"
 
 
+class Aquabub(Creature):
+    def __init__(self) -> None:
+        super().__init__("Aquabub", "Water")
+
+    def attack(self) -> str:
+        return f"{self.name} uses Water Gun!"
+
+
