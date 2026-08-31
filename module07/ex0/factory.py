c350ac8f4cd5b8dd59d0d2755f1177a03be28e8c
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-31 15:14:48 +0100

    create class CreatureFactory

--- module07/ex0/factory.py
+++ module07/ex0/factory.py
@@ -1,46 +1,13 @@
 from abc import ABC, abstractmethod
+from .creature import Creature, Flameling, Pyrodon, Aquabub, Torragon
 
 
-class Creature(ABC):
-    def __init__(self, name: str, creature_type: str) -> None:
-        self.name = name
-        self.type = creature_type
-
+class CreatureFactory(ABC):
     @abstractmethod
-    def attack(self) -> str:
+    def create_base(self) -> Creature:
         pass
 
-    def describe(self) -> str:
-        return f"{self.name} is a {self.type} type Creature"
+    @abstractmethod
+    def create_evolved(self) -> Creature:
+        pass
 
-
-class Flameling(Creature):
-    def __init__(self) -> None:
-        super().__init__("Flameling", "Fire")
-
-    def attack(self) -> str:
-        return f"{self.name} uses Ember!"
-
-
-class Pyrodon(Creature):
-    def __init__(self) -> None:
-        super().__init__("Pyrodon", "Fire/Flying")
-
-    def attack(self) -> str:
-        return f"{self.name} uses Flamethrower!"
-
-
-class Aquabub(Creature):
-    def __init__(self) -> None:
-        super().__init__("Aquabub", "Water")
-
-    def attack(self) -> str:
-        return f"{self.name} uses Water Gun!"
-
-
-class Torragon(Creature):
-    def __init__(self) -> None:
-        super().__init__("Torragon", "Water")
-
-    def attack(self) -> str:
-        return f"{self.name} uses Hydro Pump!"
