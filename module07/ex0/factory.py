d877c007ba189f7e3e2e3e4eec221179e4ef32e3
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-08-31 14:59:40 +0100

    create class Flameling

--- module07/ex0/factory.py
+++ module07/ex0/factory.py
@@ -14,3 +14,9 @@ class Creature(ABC):
         return f"{self.name} is a {self.type} type Creature"
 
 
+class Flameling(Creature):
+    def __init__(self) -> None:
+        super().__init__("Flameling", "Fire")
+
+    def attack(self) -> str:
+        return f"{self.name} use Ember!"
