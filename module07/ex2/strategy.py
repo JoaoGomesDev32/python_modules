3b5340b817d647f85db4f46c68646d1a837105c0
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-01 15:27:44 +0100

    cncrete NormalStrategy

--- module07/ex2/strategy.py
+++ module07/ex2/strategy.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from ex0.creature import Creature
+from .errors import InvalidStrategyError
 from ex1.capability import TransformCapability
 
 
@@ -14,8 +15,15 @@ class BattleStrategy(ABC):
 
 
 class NormalStrategy(BattleStrategy):
-    def attack(self) -> str:
-        pass
+    def is_valid(self, creature: Creature) -> bool:
+        return True # válida pra qualquer Creature
+
+    def act(self, creature: Creature) -> str:
+        if not self.is_valid(creature):
+            raise InvalidStrategyError(f"Invalid Creature '{creature.name}'"
+                                       f" for this normal strategy")
+        return creature.attack()
+
 
 class AggressiveStrategy(BattleStrategy):
     def attack(self) -> str:
