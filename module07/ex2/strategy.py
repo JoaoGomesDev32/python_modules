53fdf9641ae789f968a7c7578ef5519db6606a8c
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-01 15:21:33 +0100

    errors

--- module07/ex2/strategy.py
+++ module07/ex2/strategy.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
+from ex0.creature import Creature
+from ex1.capability import TransformCapability
 
 
 class BattleStrategy(ABC):
     @abstractmethod
-    def act(self) -> str:
+    def act(self, creature: Creature) -> str:
         pass
 
     @abstractmethod
-    def is_valid(self) -> bool:
+    def is_valid(self, creature: Creature) -> bool:
         pass
 
 
