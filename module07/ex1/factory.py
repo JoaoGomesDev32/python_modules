93443bbe3d6f3baf10cf64ebd6ca704da219585a
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-01 10:34:36 +0100

    class TransformCreatureFactory

--- module07/ex1/factory.py
+++ module07/ex1/factory.py
@@ -9,3 +9,11 @@ class HealingCreatureFactory(CreatureFactory):
 
     def create_evolved(self) -> Creature:
         return Bloomelle()
+
+
+class TransformCreatureFactory(CreatureFactory):
+    def create_base(self) -> Creature:
+            return Shiftling()
+    
+    def create_evolved(self) -> Creature:
+        return Morphagon()
