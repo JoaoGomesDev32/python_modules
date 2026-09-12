db9667ef1e68425774e86ba660655aa17882a32a
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-12 12:16:31 +0100

    structure AlienContact

--- module09/ex1/alien_contact.py
+++ module09/ex1/alien_contact.py
@@ -1,7 +1,22 @@
 from enum import Enum
+from pydantic import model_validator, BaseModel
 
 class ContactType(str, Enum):
     RADIO = "radio"
     VISUAL = "visual"
     PHYSICAL = "physical"
     TELEPATHIC = "telepathic"
+
+
+class AlienContact(BaseModel):
+    # ... campos ...
+
+
+    @model_validator(mode='after')
+    def validate_business_rules(self) -> 'AlienContact':
+        if not self.contact_id.startswith("AC"):
+            raise ValueError("Contact ID must start with 'AC'")
+        if self.contact_type == ContactType.PHYSICAL and not self.is_verified:
+            raise ValueError("Physical contact reports must be verified")
+        # ... as outras duas regras
+        return self
