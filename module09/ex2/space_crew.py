28b9654165bd6e5166e9cba4b116f3e941e113d9
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-17 11:14:10 +0100

    created class rank and crewmember

---
+++ module09/ex2/space_crew.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from pydantic import BaseModel, Field
+
+
+class Rank(str, Enum):
+    CADET = "cadet"
+    OFFICER = "officer"
+    LIEUTENANT = "lieutenant"
+    CAPTAIN = "captain"
+    COMMANDER = "commander"
+
+
+class CrewMember(BaseModel):
+    member_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=2, max_length=50)
+    rank: Rank
+    age: int = Field(ge=18, le=80)
+    specialization: str = Field(min_length=3, max_length=30)
+    years_experience: int = Field(ge=0, le=50)
+    is_active: bool = Field(default=True)
+
+
