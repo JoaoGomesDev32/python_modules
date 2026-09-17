9e4216093659960eba91ffd0fc5eeb2569906a78
JoaoGomesDev32 <joaogomesdev32@gmail.com>
2026-09-17 11:52:45 +0100

    created validator

--- module09/ex2/space_crew.py
+++ module09/ex2/space_crew.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from datetime import datetime
 
 
@@ -31,4 +31,27 @@ class SpaceMission(BaseModel):
     mission_status: str = Field(default="planned")
     budget_millions: float = Field(ge=1.0, le=10000.0)
 
-    
+    @model_validator(mode='after')
+    def validate_mission_rules(self) -> 'SpaceMission':
+        if not self.mission_id.startswith("M"):
+            raise ValueError("Mission ID must start with 'M'")
+        has_commander_or_captain = any(
+            member.rank in (Rank.COMMANDER, Rank.CAPTAIN)
+            for member in self.crew
+        )
+        if not has_commander_or_captain:
+            raise ValueError(
+                "Mission must have at least on Commander or Captain"
+            )
+        if self.duration_days > 365:
+            experienced_count = sum(
+                1 for member in self.crew if member.years_experience >= 5
+            )
+            total_crew = len(self.crew)
+            if experienced_count < total_crew / 2:
+                raise ValueError(
+                    "Long missions need 50% experienced crew (5+ years)"
+                )
+        if not all(member.is_active for member in self.crew):
+            raise ValueError("All crew members must be activate")
+        return self
